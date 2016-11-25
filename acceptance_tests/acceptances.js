628176309cb49d40a56890bb962c1cb67d003543
wilau2 <william.lauze@gmail.com>
2016-11-25 12:13:47 -0500

    add stuff

--- acceptance_tests/acceptances.js
+++ acceptance_tests/acceptances.js
@@ -1,41 +1,10 @@
-var english_subject = ["Practice", "meeting", "video", "training", "game", "match", "appointment", "bus", "plane", "lunch", "dinner", "breakfast", "curfew"];
-var french_subject = ["Pratique", "réunion", "vidéo", "entrainement", "parti", "match", "rendez-vous", "bus", "avion", "lunch", "souper", "diner", "déjeuner", "couvre-feu"];
-var english_actions = ["create", "cancel", "move", "update", "add", "new", "remove"];
-var french_actions = ["annuler", "nouvelle", "nouveau", "déplacer"];
-
-english_times = [
-    "tomorrow at 10",
-    "tomorrow 10pm",
-    "this wednesday",
-    "next monday",
-    "on christmas day",
-    "next week on monday at 10",
-    "tomorrow at noon",
-    "at 10 on nov 10th",
-    "10 minutes later",
-    "10 minutes earlier",
-    "minutes longer"
-];
-
-french_times = [
-    "tomorrow at 10",
-    "tomorrow 10pm",
-    "this wednesday",
-    "next monday",
-    "on christmas day",
-    "next week on monday at 10",
-    "tomorrow at noon",
-    "at 10 on nov 10th",
-    "10 minutes later",
-    "10 minutes earlier",
-    "minutes longer"
-];
-
 var assert = require('assert');
-describe('Array2', function () {
-    describe('#indexOf()', function () {
+describe('Acceptance tests', function () {
+
+    describe('', function () {
         it('should return -1 when the value is not present', function () {
             assert.equal(-1, [1, 2, 3].indexOf(4));
         });
     });
+
 });
--- gobal-const.js
+++ gobal-const.js
@@ -1,3 +1,77 @@
-/**
- * Created by williamlauze on 2016-11-25.
- */
+var english_subject = [
+    "Practice",
+    "meeting",
+    "video",
+    "training",
+    "game",
+    "match",
+    "appointment",
+    "bus",
+    "plane",
+    "lunch",
+    "dinner",
+    "breakfast",
+    "curfew"
+];
+
+var french_subject = [
+    "Pratique",
+    "réunion",
+    "vidéo",
+    "entrainement",
+    "parti",
+    "match",
+    "rendez-vous",
+    "bus",
+    "avion",
+    "lunch",
+    "souper",
+    "diner",
+    "déjeuner",
+    "couvre-feu"
+];
+var english_actions = [
+    "create",
+    "cancel",
+    "move",
+    "update",
+    "add",
+    "new",
+    "remove"
+];
+var french_actions = [
+    "annuler",
+    "nouvelle",
+    "nouveau",
+    "déplacer"
+];
+
+var english_times = [
+    "tomorrow at 10",
+    "tomorrow 10pm",
+    "this wednesday",
+    "next monday",
+    "on christmas day",
+    "next week on monday at 10",
+    "tomorrow at noon",
+    "at 10 on nov 10th",
+    "10 minutes later",
+    "10 minutes earlier",
+    "minutes longer"
+];
+
+var french_times = [
+    "demain à 10",
+    "demain à 10pm",
+    "demain à 8am",
+    "mercredi",
+    "lundi prochain",
+    "à noël",
+    "next week on monday at 10",
+    "après demain à midi",
+    "le 10 novembre à 10am",
+    "10 minutes plus tard",
+    "10 minutes plus tôt",
+    "10 minutes plus long",
+    "10 minutes plus court"
+];
