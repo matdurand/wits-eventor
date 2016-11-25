1cd3278c8b4a449c39a10d2b522e095bb3e94fe5
Mathieu Durand <mathieu.durand@ingeno.ca>
2016-11-25 11:41:26 -0500

    Added stemmer and test

---
+++ dist/test/stemmer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var assert = require('assert');
+var expect = require('chai');
+
+describe('stemmer', function () {
+    it('should throw an error for an undefined language', function () {});
+});
+//# sourceMappingURL=stemmer.js.map
---
+++ test/stemmer.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var expect = require('chai');
+
+describe('stemmer', function () {
+    it('should throw an error for an undefined language', function () {
+
+    });
+});
