8cc4fff9147fb4a7aecaa7111d3fb924db76af5e
wilau2 <william.lauze@gmail.com>
2016-11-25 11:29:26 -0500

    acceptance_tests_acceptances

--- acceptance_tests/acceptances.js
+++ acceptance_tests/acceptances.js
@@ -1,3 +1,9 @@
+var english_subject = ["Practice", "meeting", "video", "training", "game", "match", "appointment", "bus", "plane", "lunch", "dinner", "breakfast", "curfew"];
+var french_subject = ["Pratique", "réunion", "vidéo", "entrainement", "parti", "match", "rendez-vous", "bus", "avion", "lunch", "souper", "diner", "déjeuner", "couvre-feu"];
+var english_actions = ["create", "cancel", "move", "update", "add", "new", "remove"];
+var french_actions = ["annuler", "nouvelle", "nouveau", "déplacer"];
+
+
 var assert = require('assert');
 describe('Array2', function () {
     describe('#indexOf()', function () {
