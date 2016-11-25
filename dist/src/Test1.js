836f47f866ff846f566f4a39b9315028018b152a
wilau2 <william.lauze@gmail.com>
2016-11-25 11:48:40 -0500

    add dist in git ignore

--- dist/src/Test1.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-var s = '1';
-//# sourceMappingURL=Test1.js.map
--- dist/test/stemmer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var assert = require('assert');
-var expect = require('chai');
-
-describe('stemmer', function () {
-    it('should throw an error for an undefined language', function () {});
-});
-//# sourceMappingURL=stemmer.js.map
